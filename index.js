3f6daccf1da67f65a42dd178a446428b1a48d2ac
RoosaJuliaV <86317676+RoosaJuliaV@users.noreply.github.com>
2021-08-09 12:47:12 +0200

    Finished bingo board

---
+++ index.js
@@ -0,0 +1,18 @@
+const displayAllNumbers = function() {
+    let bingoContainerNode = document.getElementById("bingo-container")
+    for (let bingoNumber = 1; bingoNumber <= 76; bingoNumber++) {
+        let newBingoNumber = document.createElement("div") 
+        newBingoNumber.innerText = bingoNumber
+        newBingoNumber.classList.add("number")
+        bingoContainerNode.appendChild(newBingoNumber)
+    }
+}
+window.addEventListener("load",function() { displayAllNumbers() })
+
+const randomNumber = function() {
+    let random = Math.random() * 76
+    random = Math.floor(random)
+    let elements = document.getElementsByClassName("number")
+    let randomElement = elements[random]
+    randomElement.classList.add("selected")
+}
